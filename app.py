7999ede2d7ce9197cca8f8f357d106ef8d681b7c
giuseppesepe1997-pixel <giuseppe.sepe1997@gmail.com>
2026-04-18 14:35:28 +0200

    app.py

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import hashlib
 
 app = Flask(__name__)
+
+VERIFICATION_TOKEN = "Mhanzsecure2026asdasdasdasdasdasd"
+ENDPOINT_URL = "https://ebayserver-i0dn.onrender.com/ebay-webhook"
 
 @app.route("/")
 def home():
     return "Online"
 
-@app.route("/ebay-webhook", methods=["GET","POST"])
-def ebay():
-    return "OK"
+@app.route("/ebay-webhook", methods=["GET", "POST"])
+def ebay_webhook():
+
+    if request.method == "GET":
+        challenge_code = request.args.get("challenge_code", "")
+
+        data = challenge_code + VERIFICATION_TOKEN + ENDPOINT_URL
+        challenge_response = hashlib.sha256(data.encode()).hexdigest()
+
+        return jsonify({
+            "challengeResponse": challenge_response
+        })
+
+    return "", 200
 
 if __name__ == "__main__":
     app.run()
